924c41ba790059e700d65b02865c7bc529a157fc
Monika Mevenkamp <monikam@princeton.edu>
2019-11-14 13:15:49 -0500

    reading lines from a test file - textfile contains lines with space at beginning and end of file

--- searchFile/readlines.py
+++ searchFile/readlines.py
@@ -4,7 +4,24 @@ os.chdir('searchFile')
 
 filename = "testdata.txt"
 
-file =  open(filename, "r")
+# read lines from file
+file = open(filename, "r")
 lines = file.readlines()
 
+print("- print all lines")
 print(lines)
+print("-----------")
+
+print("")
+
+print("- print lines one by one")
+for l in lines:
+    print(l)
+print("-----------")
+
+print("")
+
+print("- print stripped lines one by one")
+for l in lines:
+    print(l.strip())
+print("-----------")
